980530849eacbe2dc2c3c35797956c2deb8b73e0
Payam <bagheri.payam@gmail.com>
2024-03-04 20:49:22 +0330

    dynamic
    made some of items in about and contact pages dynamic by using render

--- website/views.py
+++ website/views.py
@@ -6,14 +6,22 @@ from django.shortcuts import render
 def index_view(request):
     
      return render(request, 'website/index.html')
- 
+
+
+
+
+
+cont={'tel':'+971 56 808 7789', 'city':'Dubai, UAE', 'email':'bagheri.payam@gmail.com', 'job':'BI Developer &amp;  SQL Server Expert'} 
 def about_view(request):
     
-     return render(request, 'website/about.html')
+     return render(request, 'website/about.html', cont)
  
+ 
+ 
+context ={'email':'bagheri.payam@gmail.com', 'tel':'+971 56 808 7789', 'location':'Corniche, Dubai, UAE'}
 def contact_view(request):
     
-     return render(request, 'website/contact.html')
+     return render(request, 'website/contact.html', context)
  
 def education_view(request):
     
